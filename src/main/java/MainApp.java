e7f6ce5dfc68b07d67d1117fd61d740b8d92550e
Dima Maloshtan <maloshtanda@gmail.com>
2021-01-13 19:41:53 +0300

    Add methods of parse data from site

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -1,18 +1,23 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-
-import java.util.List;
+import java.io.IOException;
 
 public class MainApp {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, IOException {
+
         String cadastreNumber  = "77:01:0001062:23";
         String url = "https://pkk.rosreestr.ru";
 
-        WebDriverConfig driver = new WebDriverConfig();
+        WorkInBrowser driver = new WorkInBrowser();
+        driver.start();
         driver.navigateToSite(url);
         driver.inputAndSearchData(cadastreNumber);
         Thread.sleep(3000);
+        driver.outputData();
+
+//        Document page = Jsoup.parse(new URL(url),3000);
+//        System.out.println(page);
+
+
         driver.stop();
 
 
--- src/main/java/WorkInBrowser.java
+++ src/main/java/WorkInBrowser.java
@@ -4,21 +4,25 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
-public class WebDriverConfig {
+public class WorkInBrowser {
 
     private WebDriver driver;
     private WebDriverWait wait;
 
-    public WebDriverConfig() {
+//TODO обработать исключения
+
+    public void start() {
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        wait = new WebDriverWait(driver,10);
+        wait = new WebDriverWait(driver, 10);
     }
 
-    public void navigateToSite(String url){
+    public void navigateToSite(String url) {
         driver.navigate().to(url);
         driver.findElement(By.className("tutorial-button-outline")).click(); // выключает обучение на сайте росреестра
     }
@@ -29,9 +33,21 @@ public class WebDriverConfig {
         List<WebElement> elements = driver.findElements(By.cssSelector("svg[xmlns='http://www.w3.org/2000/svg']"));
         elements.get(82).click();
     }
+//TODO может убрать в другой класс или переименовать в этот
+
+    public void outputData() throws InterruptedException {
+        Map<WebElement, WebElement> data = new HashMap<>();
+        List<WebElement> info = driver.findElements(By.className("field-name"));
+        List<WebElement> value = driver.findElements(By.className("expanding-box_content"));
+        for(int i = 0; i<info.size(); i++){
+
+            System.out.println(info.get(i).getText() + " " + value.get(i).getText());
+        }
+    }
 
 
-    public void stop(){
+
+    public void stop() {
         driver.quit();
         driver = null;
     }
